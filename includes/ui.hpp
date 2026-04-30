db96edc0e431fedc8f467d360d8470c9d50e2073
Jalwin Grayser Jas Winston <grayserjalwin@gmail.com>
2026-04-30 17:47:35 +0530

    feat: created ui.hpp/cpp files for UI class

---
+++ includes/ui.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "includes.hpp"
+
+struct ArrayDimensions {
+    float offsetX;
+    float offsetY;
+
+    float barWidth;
+    float barHeightUnit;
+
+    float barSpacing;
+}
+
+class UIContainer {
+private:
+    ArrayDimensions arrayDimensions;
+
+public:
+    
+
+}
